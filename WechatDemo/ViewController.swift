04380c239abe281774970f8c9b1e96b810f2aab2
chaoyang805 <zhangchaoyang805@gmail.com>
2016-05-30 11:15:51 +0800

    Initial Commit

---
+++ WechatDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WechatDemo
+//
+//  Created by chaoyang805 on 16/5/30.
+//  Copyright © 2016年 jikexueyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
